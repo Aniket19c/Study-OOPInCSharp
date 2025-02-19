abb6a3fbc1f972ccfd98a64422c6847a79db146f
Aniket19c <aniketchaudhary19j@gmail.com>
2025-02-19 11:19:02 +0530

    Example of Interface

--- Study-OOPS/Program.cs
+++ Study-OOPS/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+
+interface IAnimal
+{
+    void MakeSound();
+}
+
+class Dog : IAnimal
+{
+    public string Name { get; set; }
+    public Dog(string name)
+    {
+        Name = name;
+    }
+    public void MakeSound()
+    {
+        Console.WriteLine($"{Name} barks");
+    }
+}
+
+class Cat : IAnimal
+{
+    public string Name { get; set; }
+    public Cat(string name)
+    {
+        Name = name;
+    }
+    public void MakeSound()
+    {
+        Console.WriteLine($"{Name} meows");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IAnimal myDog = new Dog("Buddy");
+        IAnimal myCat = new Cat("Whiskers");
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
